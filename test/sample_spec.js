888deb2dabb0c5c08c9c6414eaabb18e9abf1f07
Soel <shachar.soel@sap.com>
2018-07-27 20:00:15 +0300

    Sample test WIP

---
+++ test/sample_spec.js
@@ -0,0 +1,16 @@
+const { expect } = require("chai")
+const { resolve } = require("path")
+const fs = require("fs")
+const { parse } = require("../lib/parser")
+
+describe("The GraphQl advanced Parser", () => {
+  describe("Can successfully parse the official Samples", () => {
+    it("works for kitchen sink sample", () => {
+      const samplePath = resolve(__dirname, "./samples/kitchen-sink.graphql")
+      const sampleText = fs.readFileSync(samplePath, "utf8")
+      const parseResult = parse(sampleText)
+      expect(parseResult.lexErrors).to.be.empty
+      expect(parseResult.parseErrors).to.be.empty
+    })
+  })
+})

786d4ab6114f32c34fd0af79d67eabc3b4abf7bb
Shahar Soel <shahars@protonmail.com>
2018-07-28 13:46:16 +0300

    Basic benchmark

---
+++ benchmark/bench.js
@@ -0,0 +1,55 @@
+"use strict"
+
+const _ = require("lodash")
+const Benchmark = require("benchmark")
+const fs = require("fs")
+const path = require("path")
+const { parse: graphQLParse } = require("graphql/language/parser")
+const { parse: chevParse } = require("../lib/parser")
+
+const sample1 = fs
+  .readFileSync(
+    path.join(__dirname, "../test/samples/kitchen-sink.graphql"),
+    "utf8"
+  )
+  .toString()
+
+// This sample fails on the graphql-js parser...
+// Which is strange because it came from it's repo.
+const sample2 = fs
+  .readFileSync(
+    path.join(__dirname, "../test/samples/schema-kitchen-sink.graphql"),
+    "utf8"
+  )
+  .toString()
+
+let thousandLineSample = ""
+
+_.forEach(_.range(0, 18), () => {
+  thousandLineSample += sample1
+})
+
+function newSuite(name) {
+  return new Benchmark.Suite(name, {
+    onStart: () => console.log(`\n\n${name}`),
+    onCycle: event => console.log(String(event.target)),
+    onComplete: function() {
+      console.log("Fastest is " + this.filter("fastest").map("name"))
+    }
+  })
+}
+
+newSuite("GraphQL Parser Benchmark")
+  .add("grahql-js - AST output", () => graphQLParse(thousandLineSample))
+  .add("Chevrotain - CST output", () => {
+    const parseResult = chevParse(thousandLineSample)
+    if (parseResult.lexErrors.length > 0) {
+      throw "Oops"
+    }
+    if (parseResult.parseErrors.length > 0) {
+      throw "Oops"
+    }
+  })
+  .run({
+    async: false
+  })
